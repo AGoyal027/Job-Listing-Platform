520a541a023bf29deb0257d12cd8bd55ad2ad87a
AGoyal027 <ayushgoyal027@gmail.com>
2024-05-21 17:57:05 +0530

    Added logging middleware to Express server

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 
+const logStream = fs.createWriteStream(path.join(__dirname, "log.txt"), {
+    flag: 'a'
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+    const now = new Date();
+    const time = `${now.toLocaleTimeString()}`;
+    const log = `${req.method} ${req.originalUrl} ${time}`;
+    logStream.write(log + "\n");
+    next();
+})
 
 app.get('/', (req, res) => {
     res.send('Hello World').status(200);
