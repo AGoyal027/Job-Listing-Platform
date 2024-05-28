c759dd64e4398fce4810e19c6012a51eed37de7f
AGoyal027 <ayushgoyal027@gmail.com>
2024-05-28 18:24:57 +0530

    Added JWT authentication middleware and updated job routes

--- routes/job.js
+++ routes/job.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const verifyAuth = require('../middleware/verifyAuth');
 
 router.get('/', (req, res) => {
     res.status(200).send('Job Route!');
+})
+
+router.post('/create', verifyAuth, (req, res) => {
+    res.status(200).send('Created job route');
 })
 
 module.exports = router;
