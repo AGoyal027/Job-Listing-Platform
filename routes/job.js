ce74a564be1f1c89e1989fc6aa7f8696145d128c
AGoyal027 <ayushgoyal027@gmail.com>
2024-06-09 16:37:17 +0530

    Added job creation functionality and updated job routes

--- routes/job.js
+++ routes/job.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const verifyAuth = require('../middleware/verifyAuth');
+const { createJob } = require('../controllers/job')
+const { verifyAuth } = require('../middleware/verifyAuth');
 
 router.get('/', (req, res) => {
     res.status(200).send('Job Route!');
 })
 
-router.post('/create', verifyAuth, (req, res) => {
-    res.status(200).send('Created job route');
-})
+router.post('/create', verifyAuth, createJob);
 
 module.exports = router;
